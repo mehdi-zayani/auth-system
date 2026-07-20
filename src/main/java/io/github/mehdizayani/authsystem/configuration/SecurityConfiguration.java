9bd881a8da5c4a3a6aa674fe9e6945fa4394fb90
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-07-20 17:14:46 +0100

    feat(security):add Spring Security foundation

---
+++ src/main/java/io/github/mehdizayani/authsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package io.github.mehdizayani.authsystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .authorizeHttpRequests(auth -> auth
+
+                        // Swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+
+                        // Actuator
+                        .requestMatchers("/actuator/health").permitAll()
+
+                        // Public endpoints
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/**").permitAll()
+
+                        // Everything else
+                        .anyRequest().authenticated()
+                )
+
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
