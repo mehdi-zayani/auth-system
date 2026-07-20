8ae1d0b644836a9d2a52d2fcb610d846af8322b9
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-07-20 18:34:25 +0100

    feat: add custom exceptions and error response models

---
+++ src/main/java/io/github/mehdizayani/authsystem/exception/InternalServerException.java
@@ -0,0 +1,12 @@
+package io.github.mehdizayani.authsystem.exception;
+
+public class InternalServerException extends RuntimeException {
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+    public InternalServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
