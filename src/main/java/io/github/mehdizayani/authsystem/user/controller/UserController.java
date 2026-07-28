0c9085e8ce3d9734e7a2d7fe43e2fce0f791b756
Mehdi Zayani <mehdi.zayani.dev@gmail.com>
2026-07-28 16:01:06 +0100

    feat(security): implement JWT authentication

---
+++ src/main/java/io/github/mehdizayani/authsystem/user/controller/UserController.java
@@ -0,0 +1,35 @@
+package io.github.mehdizayani.authsystem.user.controller;
+
+import io.github.mehdizayani.authsystem.security.CustomUserDetails;
+import io.github.mehdizayani.authsystem.user.dto.response.UserProfileResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @GetMapping("/me")
+    public UserProfileResponse me(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+
+        return UserProfileResponse.builder()
+                .id(userDetails.getUser().getId())
+                .firstName(userDetails.getUser().getFirstName())
+                .lastName(userDetails.getUser().getLastName())
+                .username(userDetails.getUser().getUsername())
+                .email(userDetails.getUser().getEmail())
+                .enabled(userDetails.getUser().isEnabled())
+                .emailVerified(userDetails.getUser().isEmailVerified())
+                .roles(
+                        userDetails.getAuthorities()
+                                .stream()
+                                .map(authority -> authority.getAuthority())
+                                .toList()
+                )
+                .build();
+    }
+}
